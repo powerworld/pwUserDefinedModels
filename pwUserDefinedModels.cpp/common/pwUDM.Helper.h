d6b29d75b15ac64d58caf6c5e238b6af9e812164
Saurav Mohapatra <766283+sauravmohapatra@users.noreply.github.com>
2019-09-11 17:03:42 -0500

    Formatting changes, and removed DEF file from vcxproj

--- pwUserDefinedModels.cpp/common/pwUDM.Helper.h
+++ pwUserDefinedModels.cpp/common/pwUDM.Helper.h
@@ -3,33 +3,33 @@
 
 enum enumModelClass
 {
-	mcnExciter
+    mcnExciter
 };
 
 inline int pwStringCopy(wchar_t* buffer, int* bufferSize, wchar_t* string)
 {
-	size_t stringLength;
+    size_t stringLength;
 
-	stringLength = wcslen(string);
-	if ((0 < stringLength) && (stringLength <= size_t(*bufferSize)))
-	{
-		wcsncpy_s(buffer, stringLength + 1, string, stringLength);
-	}
-	return(int(stringLength));
+    stringLength = wcslen(string);    
+    if ((0 < stringLength) && (stringLength <= size_t(*bufferSize)))
+    {
+        wcsncpy_s(buffer, stringLength + 1, string, stringLength);
+    }
+    return(int(stringLength));
 }
 
 inline wchar_t* pwModelClassString(const enumModelClass udmClass)
 {
-	wchar_t* str;
+    wchar_t* str;
 
-	switch (udmClass)
-	{
-		case mcnExciter: str = L"UserDefinedExciter";
-			break;
-			// Add here in the future
-		default: str = L"UserDefinedModel";
-	}
-	return(str);
+    switch (udmClass)
+    {
+        case mcnExciter: str = L"UserDefinedExciter";
+                         break;
+        // Add here in the future
+        default: str = L"UserDefinedModel";
+    }
+    return(str);
 }
 
 #endif // PW_UDM_HELPER
--- pwUserDefinedModels.cpp/common/pwUDM.h
+++ pwUserDefinedModels.cpp/common/pwUDM.h
@@ -7,66 +7,65 @@
 
 // These types should be the same for all models and model types
 
-typedef double			TDoubleArray		[INT_MAX / 64];
-typedef int				TIntegerArray		[INT_MAX / 32];
-typedef unsigned __int8	TByteArray			[INT_MAX / 8];
-typedef	bool			TBooleanArray		[INT_MAX / 8];
-typedef char*			TStringParamsArray	[INT_MAX / 480];
+typedef double TDoubleArray[INT_MAX / 64];
+typedef int TIntegerArray[INT_MAX / 32];
+typedef unsigned __int8 TByteArray [INT_MAX / 8];
+typedef bool TBooleanArray[INT_MAX / 8];
+typedef char* TStringParamsArray[INT_MAX / 480];
 
-struct	TTxMyModelData
+struct TTxMyModelData
 {
-	TDoubleArray* FloatParams;
-	TIntegerArray* IntParams;
-	TStringParamsArray* StrParams;
-	TDoubleArray* HardCodedSignals;
-	TDoubleArray* States;
-	TBooleanArray* IgnoreStates;
-	TDoubleArray* Algebraics;
+    TDoubleArray* FloatParams;
+    TIntegerArray* IntParams;
+    TStringParamsArray* StrParams;
+    TDoubleArray* HardCodedSignals;
+    TDoubleArray* States;
+    TBooleanArray* IgnoreStates;
+    TDoubleArray* Algebraics;
 };
 
-struct	TTxSystemOptions
+struct TTxSystemOptions
 {
-	bool IgnoreLimitChecking;
-	double TimeStepSeconds;
-	double SimulationTimeSeconds;
-	double WBase;
-	double SBase;
-	double PUSolutionTolerance;
-	double MinVoltSLoad;
-	double MinVoltILoad;
+    bool IgnoreLimitChecking;
+    double TimeStepSeconds;
+    double SimulationTimeSeconds;
+    double WBase;
+    double SBase;
+    double PUSolutionTolerance;
+    double MinVoltSLoad;
+    double MinVoltILoad;
 };
 
-struct	TTxNonWindUpLimits
+struct TTxNonWindUpLimits
 {
-	TIntegerArray*	limitStates;
-	TDoubleArray*	minLimits;
-	TDoubleArray*	maxLimits;
-	TByteArray*		activeLimits;
+    TIntegerArray* limitStates;
+    TDoubleArray* minLimits;
+    TDoubleArray* maxLimits;
+    TByteArray* activeLimits;
 };
 
 struct TTxParamCounts {
-	// The Model
-	int	nFloatParams;	// N_F_PARAMS
-	int	nIntParams;		// N_I_PARAMS
-	int	nStrParams;		// N_S_PARAMS
-	int	nStates;		// N_STATES
-	int	nAlgebraics;	// N_ALGEBRAICS
-
-	// Nonwindup Limits
-	int	nNonWindUpLimits;	// N_NONWINDUPLIMITS
+    // The Model
+    int nFloatParams; // N_F_PARAMS
+    int nIntParams; // N_I_PARAMS
+    int nStrParams; // N_S_PARAMS
+    int nStates; // N_STATES
+    int nAlgebraics; // N_ALGEBRAICS
+    // Nonwindup Limits
+    int    nNonWindUpLimits; // N_NONWINDUPLIMITS
 };
 
 // These functions need to be exported in the DLL
-PW_UDM_DllExport(int)	DLLVersion();
-PW_UDM_DllExport(int)	modelClassName(int* StrSize, wchar_t* StrBuf, int dummy);
-PW_UDM_DllExport(void)	allParamCounts(TTxParamCounts* numbersOfEverything, double* timeStepSeconds);
-PW_UDM_DllExport(int)	parameterName(int* ParamNum, int* StrSize, wchar_t* StrBuf, int dummy);
-PW_UDM_DllExport(int)	stateName(int* StateNum, int* StrSize, wchar_t* StrBuf, int dummy);
-PW_UDM_DllExport(void)	getDefaultParameterValue(TTxMyModelData* ParamsAndStates);
-PW_UDM_DllExport(int)	SubIntervalPower2Exponent(TTxMyModelData* ParamsAndStates, double* timeStepSeconds);
-PW_UDM_DllExport(void)	initializeYourself(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions);
-PW_UDM_DllExport(void)	calculateFofX(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions, TTxNonWindUpLimits* nonWindUpLimits, TDoubleArray* dotX);
-PW_UDM_DllExport(void)	PropagateIgnoredStateAndInput(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions);
-PW_UDM_DllExport(int)	getNonWindUpLimits(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions, TTxNonWindUpLimits* nonWindUpLimits);
+PW_UDM_DllExport(int) DLLVersion();
+PW_UDM_DllExport(int) modelClassName(int* StrSize, wchar_t* StrBuf, int dummy);
+PW_UDM_DllExport(void) allParamCounts(TTxParamCounts* numbersOfEverything, double* timeStepSeconds);
+PW_UDM_DllExport(int) parameterName(int* ParamNum, int* StrSize, wchar_t* StrBuf, int dummy);
+PW_UDM_DllExport(int) stateName(int* StateNum, int* StrSize, wchar_t* StrBuf, int dummy);
+PW_UDM_DllExport(void) getDefaultParameterValue(TTxMyModelData* ParamsAndStates);
+PW_UDM_DllExport(int) SubIntervalPower2Exponent(TTxMyModelData* ParamsAndStates, double* timeStepSeconds);
+PW_UDM_DllExport(void) initializeYourself(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions);
+PW_UDM_DllExport(void) calculateFofX(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions, TTxNonWindUpLimits* nonWindUpLimits, TDoubleArray* dotX);
+PW_UDM_DllExport(void) PropagateIgnoredStateAndInput(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions);
+PW_UDM_DllExport(int) getNonWindUpLimits(TTxMyModelData* ParamsAndStates, TTxSystemOptions* SystemOptions, TTxNonWindUpLimits* nonWindUpLimits);
 
 #endif // PW_UDM
